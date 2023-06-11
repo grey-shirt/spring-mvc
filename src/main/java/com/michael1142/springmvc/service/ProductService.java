0c080f09a0280670909126cc2ca24717800193ba
grey-shirt <grey-shirt@hotmail.com>
2023-06-11 10:50:05 +0100

    update ProductService to make use of product repository

--- src/main/java/com/michael1142/springmvc/service/ProductService.java
+++ src/main/java/com/michael1142/springmvc/service/ProductService.java
@@ -1,24 +1,39 @@
 package com.michael1142.springmvc.service;
 
 import com.michael1142.springmvc.model.Product;
+import com.michael1142.springmvc.respositoy.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
-    private List<Product> productList = new ArrayList<>();
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     public List<Product> findAll() {
-        return productList;
+        return productRepository.findAll();
     }
 
     public void addProduct(Product p) {
-        productList.add(p);
+        productRepository.save(p);
     }
 
     public void deleteProduct(Long id) {
-        productList.removeIf((Product p) -> p.getId().equals(id));
+        productRepository.deleteById(id);
+    }
+
+    public Optional<List<Product>> findByName(String name) {
+        return productRepository.findProductByName(name);
+    }
+
+    public Optional<List<Product>> searchProducts(String search) {
+        return productRepository.findProductByNameContainingIgnoreCase(search);
     }
 }
